1dd0ce6cf0cac9f8bba015a0d0338779760d61f7
Rafael Medeiros <rafaelmedrib@gmail.com>
2023-06-22 19:16:51 -0300

    add about page boilerplate

--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -35,6 +35,14 @@ const MainLayout = ({ children }: MainLayoutProps) => {
                   Home
                 </Link>
               </li>
+              <li className="flex">
+                <Link
+                  to={routes.about()}
+                  className="-mb-1 flex items-center border-b-2 px-4 dark:border-transparent"
+                >
+                  About
+                </Link>
+              </li>
             </ul>
           </div>
           <div className="hidden flex-shrink-0 items-center lg:flex">
