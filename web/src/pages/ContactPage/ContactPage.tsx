e308dc4fb829cba3d9621091654ace4e6d1aa3ad
Rafael Medeiros <rafaelmedrib@gmail.com>
2023-06-27 20:32:44 -0300

    Add functionality to the contact page

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -4,12 +4,23 @@ import {
   FormError,
   Label,
   Submit,
+  SubmitHandler,
   TextAreaField,
   TextField,
   useForm,
 } from '@redwoodjs/forms'
 import { MetaTags, useMutation } from '@redwoodjs/web'
-import { Toaster, toast } from '@redwoodjs/web/dist/toast'
+import {
+  Toaster,
+  toast,
+  resolveValue,
+  ToastBar,
+} from '@redwoodjs/web/dist/toast'
+
+import {
+  CreateContactMutation,
+  CreateContactMutationVariables,
+} from '../../../types/graphql'
 
 const CREATE_CONTACT = gql`
   mutation CreateContactMutation($input: CreateContactInput!) {
@@ -19,26 +30,56 @@ const CREATE_CONTACT = gql`
   }
 `
 
+interface ContactFormValues {
+  name: string
+  email: string
+  message: string
+}
+
 const ContactPage = () => {
-  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+  const [create, { loading, error }] = useMutation<
+    CreateContactMutation,
+    CreateContactMutationVariables
+  >(CREATE_CONTACT, {
     onCompleted: () => {
       toast.success('Thank you for your message!')
       formMethods.reset()
     },
   })
 
-  const onSubmit = (data) => {
+  const onSubmit: SubmitHandler<ContactFormValues> = (data) => {
     create({ variables: { input: data } })
     console.log(data)
   }
 
-  const formMethods = useForm()
+  const formMethods = useForm({ mode: 'onBlur' })
 
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Toaster />
+      <Toaster>
+        {(t) => (
+          <ToastBar toast={t}>
+            {({ icon, message }) => (
+              <div className="flex max-w-2xl gap-6 divide-x divide-gray-700 overflow-hidden rounded-lg shadow-md dark:bg-gray-900 dark:text-gray-100">
+                <div className="flex flex-1 flex-col border-l-8 p-4 dark:border-violet-400">
+                  <span className="text-2xl">Success</span>
+                  <span className="text-xs dark:text-gray-400">
+                    {resolveValue(message, t)}
+                  </span>
+                </div>
+                <button
+                  onClick={() => toast.dismiss(t.id)}
+                  className="tracki flex items-center px-4 text-xs uppercase dark:border-gray-700 dark:text-gray-400"
+                >
+                  Dismiss
+                </button>
+              </div>
+            )}
+          </ToastBar>
+        )}
+      </Toaster>
       <div className="mx-auto grid max-w-screen-xl grid-cols-1 gap-8 rounded-lg px-8 py-16 dark:bg-gray-800 dark:text-gray-100 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32">
         <div className="flex flex-col justify-between">
           <div className="space-y-2">
@@ -60,11 +101,11 @@ const ContactPage = () => {
         >
           <FormError error={error} />
           <div>
-            <Label name="fullName" className="text-sm">
+            <Label name="name" className="text-sm">
               Full name
             </Label>
             <TextField
-              name="fullName"
+              name="name"
               placeholder="Leroy Jenkins"
               className="w-full rounded p-3 dark:bg-gray-800"
               validation={{
